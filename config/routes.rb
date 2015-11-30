d29ed280291d9117c34c7b87fcbbbbff381d6789
Abu el khair <mostafa.mohamed.94@hotmail.com>
2015-11-30 22:21:17 +0200

    create view for devise and test home page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  # delete "/logout" => "devise/sessions#destroy"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
