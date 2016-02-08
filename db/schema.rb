265a694983d0d0f306cac21c08d7a0060f74b0b1
Essam A. Hassan <es.hassan187@gmail.com>
2016-02-09 01:32:01 +0200

    added interests to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206185504) do
+ActiveRecord::Schema.define(version: 20160208230627) do
 
   create_table "chapters", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -39,6 +39,20 @@ ActiveRecord::Schema.define(version: 20160206185504) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  create_table "interests", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "interests_users", id: false, force: :cascade do |t|
+    t.integer "interest_id", limit: 4
+    t.integer "user_id",     limit: 4
+  end
+
+  add_index "interests_users", ["interest_id"], name: "index_interests_users_on_interest_id", using: :btree
+  add_index "interests_users", ["user_id"], name: "index_interests_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
