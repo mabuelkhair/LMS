fe942e4f3ad1b20da77ded9676d04e141f12b32e
Abu el khair <mostafa.mohamed.94@hotmail.com>
2016-06-16 11:01:51 +0200

    closes #2 add model validations and edit solution view

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,4 +1,6 @@
 class Solution < ActiveRecord::Base
 	mount_uploader :attachment, AttachmentUploader
 	belongs_to :assignment
+	validates :student_id, uniqueness: { scope: [:course_id, :assignment_id] }
+		validates :student_id,:course_id,:assignment_id, presence: true
 end
